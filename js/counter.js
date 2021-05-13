0028ab20566d04abd17b1e5f01d7b2940338a775
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2021-05-14 00:02:36 +0530

    added multiple counters

--- js/counter.js
+++ js/counter.js
@@ -1,12 +1,44 @@
-const countEl = document.getElementById('visit-counter');
+// counterAPI
+// create new link from https://api.countapi.xyz/create?namespace=fortysev-en.github.io&key={pagename}&value=0
+// check if it is working or not https://api.countapi.xyz/get/fortysev-en.github.io/{pagename}/?amount=1
+// update the counter https://api.countapi.xyz/update/fortysev-en.github.io/{pagename}/?amount=1
+// https://www.youtube.com/watch?v=R8GS-8nlekY
 
-updateVisitCount();
+const countPortfolio = document.getElementById('counter-portfolio');
+const countKeylogger = document.getElementById('counter-keylogger');
+const countAbout = document.getElementById('counter-about');
+const countCompleteBlog = document.getElementById('counter-completeblog');
 
-function updateVisitCount() {
+
+function updatePortfolioCount() {
 	fetch('https://api.countapi.xyz/update/fortysev-en.github.io/portfolio/?amount=1')
 	.then(res => res.json())
 	.then(res => {
-		countEl.innerHTML = res.value;
+		countPortfolio.innerHTML = res.value;
+	})
+}
+
+function updateKeyloggerCount() {
+	fetch('https://api.countapi.xyz/update/fortysev-en.github.io/keylogger/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countKeylogger.innerHTML = res.value;
+	})
+}
+
+function updateAboutCount() {
+	fetch('https://api.countapi.xyz/update/fortysev-en.github.io/about/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countAbout.innerHTML = res.value;
+	})
+}
+
+function updateCompleteBlogCount() {
+	fetch('https://api.countapi.xyz/update/fortysev-en.github.io/about/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countCompleteBlog.innerHTML = res.value;
 	})
 }
 
