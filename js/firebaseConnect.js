c15048bcf84b2f2fd19b809edef1d28f5054703b
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2021-05-18 12:52:45 +0530

    updated porfolio form and added date and time to the form

--- js/firebaseConnect.js
+++ js/firebaseConnect.js
@@ -10,22 +10,105 @@ var firebaseConfig = {
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
+//Get current date and time of the user
+var today = new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes();
+var dateTime = date+' '+time;
+
+// Defining Alerts
 var green_inner_html = document.querySelector(".green-inner-alert-msg");
 var red_inner_html = document.querySelector(".red-inner-alert-msg");
 const green_div = document.querySelector(".alert-popup-green");
 const red_div = document.querySelector(".alert-popup-red");
 
-// Listen for subscription form submit
+// Portfolio - Listen for the portfolio submit button
+document.getElementById("contact-form").addEventListener("submit", portfolioContact);
+
+// About - Listen for subscription form submit
 document.getElementById("subscribe-form").addEventListener("submit", updateSubscribeEmail);
 
-// Listen for suggestions form submit
+// About - Listen for suggestions form submit
 document.getElementById("suggestions-form").addEventListener("submit", updateSuggestions);
+
+//--------------------------Portfolio Contact Form--------------------------------
+function portfolioContact(event) {
+	var suggmessagesRef = firebase.database().ref("Portfolio Contact Form/" + date);
+
+	event.preventDefault();
+
+	var portemail = getInputVal("portemail");
+	var portname = getInputVal("portname");
+	var portmessage = getInputVal("portmessage");
+	var resetForm = document.getElementById("contact-form");
+	var text;
+
+	if (portname == "" || portname.length < 5) {
+		text = "Please enter a valid name!";
+		red_inner_html.innerHTML = text;
+		red_div.style.display = "block";
+		setTimeout(function () {
+			red_div.style.display = "none";
+		}, 5000);
+		return false;
+	}
+
+	if (
+		portemail == "" ||
+		portemail.indexOf("@") == -1 ||
+		portemail.length < 6
+	) {
+		text = "Please enter a valid email address!";
+		red_inner_html.innerHTML = text;
+		red_div.style.display = "block";
+		setTimeout(function () {
+			red_div.style.display = "none";
+		}, 5000);
+		return false;
+	}
+
+	if (portmessage == "" || portmessage.length <= 10) {
+		text = "Some more information would be better!";
+		red_inner_html.innerHTML = text;
+		red_div.style.display = "block";
+		setTimeout(function () {
+			red_div.style.display = "none";
+		}, 5000);
+		return false;
+	} else {
+		saveMessage(portemail, portname, portmessage, dateTime);
+		resetForm.reset();
+		text = "Thank you for reaching out to me. I'll get back to you within 48 hours!";
+		green_inner_html.innerHTML = text;
+		green_div.style.display = "block";
+		setTimeout(function () {
+			green_div.style.display = "none";
+		}, 5000);
+	}
+
+	// Function to get get form values
+	function getInputVal(id) {
+	return document.getElementById(id).value;
+	}
+
+	// Save message to firebase
+	function saveMessage(portemail, portname, portmessage, dateTime) {
+		var newsuggMessageRef = suggmessagesRef.push();
+		newsuggMessageRef.set({
+			Name: portname,
+			Email: portemail,
+			Message: portmessage,
+			Date: dateTime
+		});
+	}
+}
+
 
 //---------------------Subscription Email Form-------------------------------------
 
 function updateSubscribeEmail(event) {
 	// Reference messages collection
-	var messagesRef = firebase.database().ref("Subscription Emails");
+	var messagesRef = firebase.database().ref("Subscription Emails/" + date);
 	var resetForm = document.getElementById("subscribe-form");
 	
 	// To avoid entire page getting refreshed
@@ -43,7 +126,7 @@ function updateSubscribeEmail(event) {
 			red_div.style.display = "none";
 		}, 5000);
 	} else {
-		saveMessage(email);
+		saveMessage(email, dateTime);
 		resetForm.reset();
 		text = "You have successfully subscribed!";
 		green_inner_html.innerHTML = text;
@@ -59,17 +142,18 @@ function updateSubscribeEmail(event) {
 	}
 
 	// Save message to firebase
-	function saveMessage(email) {
+	function saveMessage(email, dateTime) {
 		var newMessageRef = messagesRef.push();
 		newMessageRef.set({
-			Email: email
+			Email: email,
+			Date: dateTime
 		});
 	}
 }
 
 //---------------------------------Suggestions Form--------------------------------------------
 function updateSuggestions(event) {
-	var suggmessagesRef = firebase.database().ref("Suggestions Form");
+	var suggmessagesRef = firebase.database().ref("Suggestions Form/" + date);
 
 	event.preventDefault();
 
@@ -112,7 +196,7 @@ function updateSuggestions(event) {
 		}, 5000);
 		return false;
 	} else {
-		saveMessage(suggemail, suggname, suggmessage);
+		saveMessage(suggemail, suggname, suggmessage, dateTime);
 		resetForm.reset();
 		text = "Thank you for your suggestion!";
 		green_inner_html.innerHTML = text;
@@ -128,12 +212,13 @@ function updateSuggestions(event) {
 	}
 
 	// Save message to firebase
-	function saveMessage(suggemail, suggname, suggmessage) {
+	function saveMessage(suggemail, suggname, suggmessage, dateTime) {
 		var newsuggMessageRef = suggmessagesRef.push();
 		newsuggMessageRef.set({
 			Name: suggname,
 			Email: suggemail,
 			Message: suggmessage,
+			Date: dateTime
 		});
 	}
 }
