168d7832a28e28dfe309ade6881148fc2b9bed17
Pushkar Jadhao <pushkar.jadhao@gmail.com>
2021-05-18 12:12:04 +0530

    fixed forms backend from about page

--- js/firebaseConnect.js
+++ js/firebaseConnect.js
@@ -1,61 +1,56 @@
 var firebaseConfig = {
-	apiKey: "AIzaSyAk0xzpUuH0LYXihigz3OXentCn1T8YC3Q",
-	authDomain: "global-testing-env.firebaseapp.com",
-	databaseURL: "https://global-testing-env-default-rtdb.firebaseio.com",
-	projectId: "global-testing-env",
-	storageBucket: "global-testing-env.appspot.com",
-	messagingSenderId: "403456253125",
-	appId: "1:403456253125:web:129423ef3a31c45a5a24ac",
+	apiKey: "AIzaSyBjk1Yl311XbB-028MzZ_lxMHgsztLz7dg",
+	authDomain: "fortyseven-contact-form-deb4b.firebaseapp.com",
+	databaseURL: "https://fortyseven-contact-form-deb4b.firebaseio.com",
+	projectId: "fortyseven-contact-form-deb4b",
+	storageBucket: "fortyseven-contact-form-deb4b.appspot.com",
+	messagingSenderId: "935932983038",
+	appId: "1:935932983038:web:b819c53f9f05a14e4617af",
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 
-// // ---------------------Subscription Email Form-------------------------------------
-function updateSubscribeEmail() {
+var green_inner_html = document.querySelector(".green-inner-alert-msg");
+var red_inner_html = document.querySelector(".red-inner-alert-msg");
+const green_div = document.querySelector(".alert-popup-green");
+const red_div = document.querySelector(".alert-popup-red");
+
+// Listen for subscription form submit
+document.getElementById("subscribe-form").addEventListener("submit", updateSubscribeEmail);
+
+// Listen for suggestions form submit
+document.getElementById("suggestions-form").addEventListener("submit", updateSuggestions);
+
+//---------------------Subscription Email Form-------------------------------------
+
+function updateSubscribeEmail(event) {
 	// Reference messages collection
 	var messagesRef = firebase.database().ref("Subscription Emails");
+	var resetForm = document.getElementById("subscribe-form");
+	
+	// To avoid entire page getting refreshed
+	event.preventDefault();
 
-	// Listen for form submit
-	document
-		.getElementById("subscribe-form")
-		.addEventListener("submit", submitForm);
+	// Get values
+	var email = getInputVal("email");
 
-	// Submit form
-	function submitForm(e) {
-		// To avoid entire page getting refreshed
-		e.preventDefault();
-
-		// Get values
-		var email = getInputVal("email");
-		var resetForm = document.getElementById("subscribe-form");
-		var green_inner_html = document.querySelector(".green-inner-alert-msg");
-		var red_inner_html = document.querySelector(".red-inner-alert-msg");
-		const green_div = document.querySelector(".alert-popup-green");
-		const red_div = document.querySelector(".alert-popup-red");
-		var text;
-
-		setTimeout(() => { 
-			if (email == "" || email.indexOf("@") == -1 || email.length < 6) {
-			text = "Please enter a valid email address!";
-			red_inner_html.innerHTML = text;
-			red_div.style.display = "block";
-			setTimeout(function () {red_div.style.display = "none";
-			}, 6000);
-			return false;
-			} 
-		else {
-			// Save message
-			saveMessage(email);
-			resetForm.reset();
-			text = "You have successfully subscribed!";
-			green_inner_html.innerHTML = text;
-			green_div.style.display = "block";
-			setTimeout(function () {
-				green_div.style.display = "none";
-			}, 6000);
-			} 
-		}, 1000);
-		
+	var text;
+	if (email.indexOf("@") === -1 || email.length < 6) {
+		text = "Please enter a valid email address!";
+		red_inner_html.innerHTML = text;
+		red_div.style.display = "block";
+		setTimeout(function () {
+			red_div.style.display = "none";
+		}, 5000);
+	} else {
+		saveMessage(email);
+		resetForm.reset();
+		text = "You have successfully subscribed!";
+		green_inner_html.innerHTML = text;
+		green_div.style.display = "block";
+		setTimeout(function () {
+			green_div.style.display = "none";
+		}, 5000);
 	}
 
 	// Function to get get form values
@@ -67,41 +62,69 @@ function updateSubscribeEmail() {
 	function saveMessage(email) {
 		var newMessageRef = messagesRef.push();
 		newMessageRef.set({
-			Email: email,
+			Email: email
 		});
 	}
 }
 
 //---------------------------------Suggestions Form--------------------------------------------
-function updateSuggestions() {
+function updateSuggestions(event) {
 	var suggmessagesRef = firebase.database().ref("Suggestions Form");
 
-	document.getElementById("suggestions-form").addEventListener("submit", submitForm);
+	event.preventDefault();
 
-	function submitForm(e) {
-		e.preventDefault();
+	var suggemail = getInputVal("sugemail");
+	var suggname = getInputVal("sugname");
+	var suggmessage = getInputVal("sugmessage");
+	var resetForm = document.getElementById("suggestions-form");
+	var text;
 
-		var suggemail = getInputVal("sugemail");
-		var suggname = getInputVal("sugname");
-		var suggmessage = getInputVal("sugmessage")
-		var resetForm = document.getElementById("suggestions-form");
+	if (suggname == "" || suggname.length < 5) {
+		text = "Please enter a valid name!";
+		red_inner_html.innerHTML = text;
+		red_div.style.display = "block";
+		setTimeout(function () {
+			red_div.style.display = "none";
+		}, 5000);
+		return false;
+	}
 
+	if (
+		suggemail == "" ||
+		suggemail.indexOf("@") == -1 ||
+		suggemail.length < 6
+	) {
+		text = "Please enter a valid email!";
+		red_inner_html.innerHTML = text;
+		red_div.style.display = "block";
+		setTimeout(function () {
+			red_div.style.display = "none";
+		}, 5000);
+		return false;
+	}
+
+	if (suggmessage == "" || suggmessage.length <= 10) {
+		text = "I'll need more information duh! Add some more content.";
+		red_inner_html.innerHTML = text;
+		red_div.style.display = "block";
+		setTimeout(function () {
+			red_div.style.display = "none";
+		}, 5000);
+		return false;
+	} else {
 		saveMessage(suggemail, suggname, suggmessage);
 		resetForm.reset();
-		// Show alert
-		document.querySelector(".inner-alert-msg").innerHTML =
-			"Thank you for your suggestion! &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;";
-		document.querySelector(".alert-popup-green").style.display = "block";
-
-		// Hide alert after 3 seconds
+		text = "Thank you for your suggestion!";
+		green_inner_html.innerHTML = text;
+		green_div.style.display = "block";
 		setTimeout(function () {
-			document.querySelector(".alert-popup-green").style.display = "none";
+			green_div.style.display = "none";
 		}, 5000);
 	}
 
 	// Function to get get form values
 	function getInputVal(id) {
-		return document.getElementById(id).value;
+	return document.getElementById(id).value;
 	}
 
 	// Save message to firebase
@@ -110,7 +133,7 @@ function updateSuggestions() {
 		newsuggMessageRef.set({
 			Name: suggname,
 			Email: suggemail,
-			Message: suggmessage
+			Message: suggmessage,
 		});
 	}
 }
